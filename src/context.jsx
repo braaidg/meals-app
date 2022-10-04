f7bb99d63490e98148f9b7a1241a48f59bc8d20e
braaidg <braiandenisgomez@gmail.com>
2022-10-03 21:57:28 -0300

    Meals: added Loading case and No meals case

--- src/context.jsx
+++ src/context.jsx
@@ -8,15 +8,22 @@ const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a"
 const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false)
     const [meals, setMeals] = useState([]);
 
   const fetchMeals = async (url) => {
+    setLoading(true)
     try {
       const { data } = await axios(url)
-      setMeals(data.meals);
+      if (data.meals) {
+        setMeals(data.meals)
+      } else {
+        setMeals([])
+      }
     } catch (error) {
       console.log(error.response);
     }
+    setLoading(false)
   };
 
   useEffect(() => {
@@ -24,7 +31,7 @@ const AppProvider = ({ children }) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{meals}}>
+    <AppContext.Provider value={{meals, loading}}>
       {children}
     </AppContext.Provider>
   );
